7bd96d9ae238fcf3be1eb54535433bc8034f1d38
JNS <65025344+jinto4638@users.noreply.github.com>
2020-07-07 21:03:58 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,17 +1,18 @@
 class Translation(object):
-    START_TEXT = """Hello....\n
-I'm FC telegram bot bot maintained by  [this person](tg://user?id={}) .\n\n
-You can convert any Telegram Document to streamable video and vice versa, using this bot! With custom thumbnail
-     you can check more /help
+    START_TEXT = """Hello.... 🙋🏻‍♀️ \n
+I'm FC telegram bot bot maintained by  [this person](https://t.me/jintons) .\n\n
+You can convert any Telegram Document to streamable video using this bot! With custom thumbnail
+<b>send image to set custom thumbnail which is valid for 24Hr</b>
+    \n you can check more /help
      
     """
     ABS_TEXT = " Please don't be selfish."
     BANNED_USER_TEXT = "you are banned"
     UPGRADE_TEXT = "No upgrade plan 🥰"
-    DOWNLOAD_START = "trying to download"
-    UPLOAD_START = "trying to upload"
+    DOWNLOAD_START = " ⌛️ ⏳ trying to download.."
+    UPLOAD_START = "trying to upload 📤 "
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "https://t.me/fileConvertRobot"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "https://t.me/fcfilmcornercc join for latest movies"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nPlease rate me if you find me useful. https://t.me/tlgrmcbot?start=anydl_bot-bot \nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in next 24 hr"
@@ -23,7 +24,7 @@ You can convert any Telegram Document to streamable video and vice versa, using 
     CURENT_PLAN_DETAILS = """Current plan details
 --------
 Telegram ID: <code>{}</code>
-Plan name: <a href='https://t.me/InFoTelGroup/174'>{}</a>
+Plan name: Nill
 Expires on: {}"""
     HELP_USER = """I can do: Convert To Streamable video any telegram file
 --------
@@ -40,4 +41,4 @@ Expires on: {}"""
 
 """
 
-    REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
+    REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert 🙆🏻‍♀️ "
